3ccb2546211ec7c89bf2bc711344e1df2b16e5d3
csj2702 <csj2702@naver.com>
2019-06-25 19:45:16 +0900

    study

---
+++ Search/HamiltonianCycle.java
@@ -0,0 +1,51 @@
+package Search;
+
+public class HamiltonianCycle {
+
+	private int numOfVertexes;
+	private int[] hamiltonianPath;
+	private int[][] adjacencyMatrix;
+
+	public HamiltonianCycle(int[][] adjacencyMatrix) {
+		this.adjacencyMatrix = adjacencyMatrix;
+		this.hamiltonianPath = new int[adjacencyMatrix.length];
+		this.numOfVertexes = adjacencyMatrix.length;
+
+		this.hamiltonianPath[0] = 0;
+
+		if (findFeasibleSolution(1)) {
+			showHamiltonianPath();
+		} else {
+			System.out.println("no feasible soltuon");
+		}
+	}
+
+	private void showHamiltonianPath() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private boolean findFeasibleSolution(int position) {
+
+		if (position == numOfVertexes) {
+			if (adjacencyMatrix[hamiltonianPath[position - 1]][hamiltonianPath[0]] == 1) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		for (int vertexIndex = 1; vertexIndex < numOfVertexes; ++vertexIndex) {
+
+			if (isFeasible(vertexIndex, position)) {
+
+				hamiltonianPath[position] = vertexIndex;
+
+				if (findFeasibleSolution(position + 1)) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
